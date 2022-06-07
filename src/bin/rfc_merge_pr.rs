a2cd1cdb6dc251a499b9b4ecf6d222d8beb758c9
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-06-07 00:58:59 -0400

    revised instrumentation to try to get more info about what's going wrong.

--- src/bin/rfc_merge_pr.rs
+++ src/bin/rfc_merge_pr.rs
@@ -1,4 +1,4 @@
-use triagebot::{github, logger, rfc_merge_pr};
+use triagebot::{github, rfc_merge_pr};
 
 use anyhow::{Context};
 use reqwest::{Client};
@@ -6,7 +6,7 @@ use reqwest::{Client};
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     dotenv::dotenv().ok();
-    logger::init();
+    tracing_subscriber::fmt::init();
 
     let oc = octocrab::OctocrabBuilder::new()
         .personal_token(github::default_token_from_env())
@@ -18,10 +18,10 @@ async fn main() {
         panic!("{f} expected first argument, an RFC PR # to merge.", f=f);
     });
     let arg: u64 = arg.parse().unwrap_or_else(|e| {
-        panic!("{f} expected numeric first argument, but it failed to parse; {e}", f=f, e=e);
+        panic!("{f} expected numeric first argument, but it failed to parse; {e:?}", f=f, e=e);
     });
 
     rfc_merge_pr::merge(arg).await.unwrap_or_else(|e| {
-        panic!("{f} failure during merge: {e}", f=f, e=e);
+        panic!("{f} failure during merge: {e:?}", f=f, e=e);
     });
 }
